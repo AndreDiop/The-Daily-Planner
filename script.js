c3f6624a1d20021eb46242dd76e28d5eb51131a2
Andre Diop <itsjustandre@gmail.com>
2020-12-16 22:05:10 -0500

    one row on screen, adding formatting now

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-var currentTime = Date
+// Create an element 
+var row = $("<div>");
+row.addClass("row time-block");
+
+var timeEl = $("<div>");
+row.addClass("hour col");
+row.append(timeEl);
+
+
+var textArea = $("<textarea>");
+textArea.addClass("past description col-10");
+row.append(textArea);
+
+
+var button = $("<button>");
+button.addClass("saveBtn btn btn-secondary col");
+row.append(button);
+
+$(".container").append(row);
+
+
+
+// add some content
+
+
+
+
+
+// append to the document
