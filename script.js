9055deaab1017e809cc8907fcbcc99c144746903
Andre Diop <itsjustandre@gmail.com>
2020-12-17 10:00:25 -0500

    added current time to time slot

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-// Create an element 
-var row = $("<div>");
-row.addClass("row time-block");
+// Create an element
+var hoursArray = [
+  "0900",
+  "1000",
+  "1100",
+  "1200",
+  "1300",
+  "1400",
+  "1500",
+  "1600",
+  "1700",
+];
+var presentTime = moment();
 
-var timeEl = $("<div>");
-row.addClass("hour col");
-row.append(timeEl);
+for (var i = 0; i < hoursArray.length; i++) {
+  console.log(hoursArray[i]);
+}
 
+function createRow() {
+  var row = $("<div>");
+  row.addClass("row time-block");
 
-var textArea = $("<textarea>");
-textArea.addClass("past description col-10");
-row.append(textArea);
+  var hour = $("<div>");
+  row.addClass("hour col-auto");
+  row.text("     "+hoursArray[0]+"     ");
+  row.append(hour);
 
+  var textArea = $("<textarea>");
+  textArea.addClass("description col-10");
+  row.append(textArea);
 
-var button = $("<button>");
-button.addClass("saveBtn btn btn-secondary col");
-row.append(button);
+  var button = $("<button>");
+  button.addClass("saveBtn btn col");
+  button.text("Save Info");
+  row.append(button);
 
-$(".container").append(row);
+  $(".container").append(row);
+}
 
-
+createRow();
 
 // add some content
-
-
-
-
 
 // append to the document
