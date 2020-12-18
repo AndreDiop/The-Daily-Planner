7e44e2023061e8319c46b1106f4706cf3106aab9
Andre Diop <itsjustandre@gmail.com>
2020-12-17 21:36:16 -0500

    rows now change color based on time of day

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-var hoursArray = [
-    "0900",
-    "1000",
-    "1100",
-    "1200",
-    "1300",
-    "1400",
-    "1500",
-    "1600",
-    "1700",
-  ];
-  
-  var timeNow = moment().format("HHmm");
-  
-  
-  function createRow() {
-    for (var i = 0; i < hoursArray.length; i++) {
-  
-      var row = $("<div>");
-      row.addClass("row time-block " + hoursArray[i]);
-  
-      var hour = $("<div>");
-      row.addClass("hour col-auto");
-      row.text("     " + hoursArray[i] + "     ");
-  
-      var textArea = $("<textarea>");
-      textArea.addClass("description col");
-  
-      var button = $("<button>");
-      button.addClass("saveBtn btn col-auto");
-      button.text("Save Info");
-      // row.append(button);
-      row.append(hour).append(textArea).append(button);
-      $(".container").append(row);
-      if (hoursArray[i] === timeNow) {
-        textArea.addClass("present");
-      } else hoursArray[i] > timeNow;
+var hoursArray = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+var timeNow = moment().format("HH");
+
+function createRow() {
+  for (var i = 0; i < hoursArray.length; i++) {
+    var row = $("<div>");
+    row.addClass("row time-block " + hoursArray[i]);
+
+    var hour = $("<div>");
+    row.addClass("hour col-auto");
+    row.text("     " + hoursArray[i] + "00   ");
+
+    var textArea = $("<textarea>");
+    textArea.addClass("description col");
+
+    var button = $("<button>");
+    button.addClass("saveBtn btn col-auto");
+    button.text("Save Info");
+    // row.append(button);
+    row.append(hour).append(textArea).append(button);
+    $(".container").append(row);
+    if (timeNow < hoursArray[i]) {
       textArea.addClass("future");
+    } else if (timeNow > hoursArray[i]) {
+      textArea.addClass("past");
+    } else if (timeNow === hoursArray[i]) {
+      textArea.addClass("present");
     }
   }
-  
-  createRow();
-  
+}
+
+createRow();
